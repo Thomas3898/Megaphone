699a8b52c964d3208e80a626209e19f6b036cca8
Thomas <grandin.thoma@gmail.com>
2022-04-22 11:37:49 +0200

    Test vocal plus précis

--- Assets/Vocal.cs
+++ Assets/Vocal.cs
@@ -9,25 +9,50 @@ public class Vocal : MonoBehaviour
 {
     private Dictionary<string, Action> keyWordActions = new Dictionary<string, Action>();
     private KeywordRecognizer keywordRecognizer;
+    private PhraseRecognizer phraseRecognizer;
     public GameObject character;
+    private DictationRecognizer dictationRecognizer;
     // Start is called before the first frame update
     void Start()
     {
+        //dictationRecognizer = new DictationRecognizer();
+       // dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
         keyWordActions.Add("Enflamme toi", Flamme);
+        
 
-        keywordRecognizer = new KeywordRecognizer(keyWordActions.Keys.ToArray());
-        keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognized;
+        keywordRecognizer = new KeywordRecognizer(keyWordActions.Keys.ToArray(), ConfidenceLevel.High);
+        keywordRecognizer.OnPhraseRecognized += OnPhraseRecognized;
         keywordRecognizer.Start();
+        //dictationRecognizer.Start();
+        //dictationRecognizer.DictationResult -= DictationRecognizer_DictationResult;
+        //dictationRecognizer.Dispose();
     }
 
-    private void OnKeywordsRecognized(PhraseRecognizedEventArgs args)
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
-        keyWordActions[args.text].Invoke();
+        System.Action keywordAction;
+        if (keyWordActions.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+        /*keyWordActions[args.text].Invoke();
+        Debug.Log(args.text);*/
     }
+
+    /*private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
+    {
+        Debug.Log(text);
+        if (text == "enflamme toi")
+        {
+            Flamme();
+        }
+        
+    }*/
 
     void Flamme()
     {
         Debug.Log("wtfSaFonctionne");
+        
     }
 
     // Update is called once per frame
